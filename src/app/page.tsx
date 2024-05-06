5602e9a13990d4d1e9b7c15ac476e1727ac86cf1
Fazt <fazt@faztweb.com>
2024-05-06 11:00:56 -0500

    dynamic added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
-
 import { TaskCard } from "@/components/task-card";
 import prisma from "@/lib/prisma";
+
+export const dynamic = "force-dynamic";
 
 async function HomePage() {
   const tasks = await prisma.task.findMany();
@@ -8,7 +9,7 @@ async function HomePage() {
   return (
     <div className="grid grid-cols-3 gap-4">
       {tasks.map((task) => (
-       <TaskCard task={task} key={task.id} /> 
+        <TaskCard task={task} key={task.id} />
       ))}
     </div>
   );
